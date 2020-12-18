f7ca76fa914f81416299e61ef5a374b1d7dc3578
fsafdsa Lfybkf <P.danila228@mail.ru>
2020-12-18 18:20:44 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2222/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine(" 'a' enter");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("'b'  enter");
+                double b = double.Parse(Console.ReadLine());
+                double p = a + b + Math.Sqrt(a * a + b * b);
+                double s = a * b / 2.0;
+                Console.WriteLine("Square " + s);
+                Console.ReadLine();
+            }
+        }
+    }
+}
